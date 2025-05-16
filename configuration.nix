48968824831a9b3466fa2781bc35d6d50d50f18c
mrfufl4ik <mrfufl4ik@gmail.com>
2025-05-16 19:26:33 +0600

    tabs problem

--- configuration.nix
+++ configuration.nix
@@ -1,14 +1,9 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
 { config, lib, pkgs, ... }:
-
 {
   imports = [
-		./hardware-configuration.nix
-		./packages.nix
-		./modules/bootstrap.nix
+    ./hardware-configuration.nix 
+    ./packages.nix 
+    ./modules/bootstrap.nix
 	];
 
   networking.hostName = "nixos";
